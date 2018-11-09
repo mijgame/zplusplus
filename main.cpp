d2f4160cfdb900c080e8bf6b75f8fce8cc428798
Lex Ruesink <lexruesink@live.nl>
2018-11-09 11:11:49 +0100

    Initial

---
+++ main.cpp
@@ -0,0 +1,61 @@
+#include "zplusplus.hpp"
+
+#include <iostream>
+#include <string>
+
+functie leegte schrijf(constante karakterketting referentie tekst) {
+    standaarduitvoer schuif naar links tekst;
+}
+
+moeilijk gedoe<tiepenaam T>
+functie leegte doe_iets() {
+
+}
+
+klasse taak {};
+
+klasse vlag {
+beschermd:
+    taak verwijzing t = nulverwijzing;
+
+publiek:
+    vlag(taak verwijzing t)
+        : t(t) {};
+};
+
+nummer hoofdlus() {
+    niet ondertekend nummer foo = 69;
+    zwevend punt x = 4.f;
+    karakterketting tekst = "geweldig";
+
+    gar c = 'a';
+
+    taak t;
+    vlag v(&t);
+
+    als ((&t) gelijk aan nulverwijzing) {
+
+    } anders als (1 gelijk aan 2) {
+
+    } anders als ('a' ongelijk aan 'a') {
+
+    } anders {
+
+    };
+
+    nummer n = 0;
+    boel conditie = onwaar;
+    terwijl lus (conditie ongelijk aan waar) {
+        als (n gelijk aan 15) {
+            conditie = waar;
+        }
+
+        n += 1;
+    }
+
+    voor lus (nummer i = 0; i kleiner dan 10; i++ ) {
+        schrijf("Hallo");
+    }
+
+    geef 0 terug;
+}
---
+++ zplusplus.hpp
@@ -0,0 +1,91 @@
+#ifndef ZPLUSPLUS_ZPLUSPLUS_HPP
+#define ZPLUSPLUS_ZPLUSPLUS_HPP
+
+// Functions
+#define hoofdlus main
+
+// Types
+// Integer
+#define nummer int
+#define zwevend
+
+// Floating
+#define punt float
+#define dubbel double
+
+// Unsigned
+#define niet
+#define ondertekend unsigned
+
+// Modifiers
+#define constant const
+#define constante const
+
+// Pointers/refs
+#define verwijzing *
+#define referentie &
+#define nulverwijzing nullptr
+
+// Chars
+#define gar char
+
+#define boel bool
+#define waar true
+#define onwaar false
+
+#define leegte void
+
+// Strings
+#define karakterketting std::string
+
+// Return
+#define geef return
+#define terug
+
+// Structures
+#define structuur struct
+#define klasse class
+
+// Access modifiers
+#define publiek public
+#define beschermd protected
+#define prive private
+
+// Control flow
+#define als if
+#define anders else
+
+#define gelijk ==
+#define ongelijk !=
+#define aan
+#define dan
+#define of
+
+#define kleiner <
+#define groter >
+
+// Loops
+#define lus
+#define terwijl while
+#define voor for
+
+#define vluchtig volatile
+
+// Standard
+#define standaard std
+#define uitvoer cout
+#define invoer cin
+#define standaarduitvoer std::cout
+#define standaardinvoer std::cin
+#define schuif
+#define naar
+#define links <<
+#define rechts >>
+
+#define functie
+
+#define moeilijk
+#define gedoe template
+#define tiepenaam typename
+
+#endif //ZPLUSPLUS_ZPLUSPLUS_HPP
